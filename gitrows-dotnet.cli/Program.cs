cb4b28fc5b047db5a482ea485ad6f98197c0aae5
Charlie <charlie.tizzard@polystream.com>
2023-06-05 20:29:14 +0100

    clean up

--- gitrows-dotnet.cli/Program.cs
+++ gitrows-dotnet.cli/Program.cs
@@ -4,7 +4,7 @@
     {
         public static async Task Main()
         {
-            var client = new Gitrows(user: "8BitSensei", token: "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");
+            var client = new Gitrows(user: "", token: "");
             var result = await client.Delete("@github/8BitSensei/Templum-Data#testing/data/templum_sites.json", new Dictionary<string, string> { {"index", "eq:2" } });
         }
     }
--- gitrows-dotnet/Gitrows.cs
+++ gitrows-dotnet/Gitrows.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
 
 namespace gitrows_dotnet
 {
